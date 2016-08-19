7154059f8d4586ab85da082984fa7b9c55abe357
Vasya Makarchuk <vasyamakarchuk@outlook.com>
2016-08-19 14:04:44 +0300

    auth servise is ready

--- Deep/Auth/AuthService.svc.cs
+++ Deep/Auth/AuthService.svc.cs
@@ -7,8 +7,7 @@ using System.Text;
 
 namespace Deep.Auth
 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "AuthService" in code, svc and config file together.
-    // NOTE: In order to launch WCF Test Client for testing this service, please select AuthService.svc or AuthService.svc.cs at the Solution Explorer and start debugging.
+   
     public class AuthService : IAuthService
     {
         DeepEntities context = new DeepEntities();
@@ -30,15 +29,93 @@ namespace Deep.Auth
                     AccessToken token = context.AccessTokens.Where(p => p.id == user.id).FirstOrDefault();
                     if (token == null)
                     {
-                        
-                        
+                        token = new AccessToken();
+                        token.id = user.id;
+                        token.access_token = Convert.ToBase64String(Guid.NewGuid().ToByteArray());
+                        token.refresh_token = Convert.ToBase64String(Guid.NewGuid().ToByteArray());
+                        token.last_refresh = DateTime.Now;
+
+                        context.AccessTokens.Add(token);
+                        context.SaveChanges();
                     }
+                    else
+                    {
+                        token.access_token = Convert.ToBase64String(Guid.NewGuid().ToByteArray());
+                        token.refresh_token = Convert.ToBase64String(Guid.NewGuid().ToByteArray());
+                        token.last_refresh = DateTime.Now;
+
+                        context.SaveChanges();
+                    }
+                    auth_body.access_token = token.access_token;
+                    auth_body.refresh_token = token.refresh_token;
+                    auth_body.exception = null;
+                    
                     return auth_body;
                 }
             }
             catch(Exception ex)
             {
+                auth_body.access = false;
                 auth_body.exception = ex.Message;
+                return auth_body;
+            }
+        }
+
+        public AuthBody Authentication(int user_id, string access_token)
+        {
+            AuthBody auth_body = new AuthBody();
+            try
+            {
+                AccessToken token = context.AccessTokens.Where(p => p.id == user_id).Where(p => p.access_token == access_token).FirstOrDefault();
+                if (token == null)
+                {
+                    auth_body.exception = "Access denied!";
+                    auth_body.access = false;
+                }
+                else
+                {
+                    auth_body.exception = null;
+                    auth_body.access = true;
+                }
+                return auth_body;
+            }
+            catch(Exception ex)
+            {
+                auth_body.access = false;
+                auth_body.exception = ex.Message;
+                return auth_body;
+            }
+        }
+
+        public AuthBody UpdateToken(int user_id, string access_token, string refresh_token)
+        {
+            AuthBody auth_body = new AuthBody();
+            try
+            {
+                AccessToken token = context.AccessTokens.Where(p => p.id == user_id).Where(p => p.access_token == access_token).Where(p => p.refresh_token == refresh_token).FirstOrDefault();
+                if (token == null)
+                {
+                    auth_body.exception = "Some mistake";
+                    auth_body.access_token = null;
+                    auth_body.refresh_token = null;
+                }
+                else
+                {
+                    token.access_token = Convert.ToBase64String(Guid.NewGuid().ToByteArray());
+                    token.refresh_token = Convert.ToBase64String(Guid.NewGuid().ToByteArray());
+                    token.last_refresh = DateTime.Now;
+
+                    context.SaveChanges();
+
+                    auth_body.refresh_token = token.refresh_token;
+                    auth_body.access_token = token.access_token;
+                }
+                return auth_body;
+            }
+            catch (Exception ex)
+            {
+                auth_body.exception = ex.Message;
+                auth_body.access = false;
                 return auth_body;
             }
         }
--- Deep/Auth/IAuthService.cs
+++ Deep/Auth/IAuthService.cs
@@ -7,10 +7,17 @@ using System.Text;
 
 namespace Deep.Auth
 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IAuthService" in both code and config file together.
+    
     [ServiceContract]
     public interface IAuthService
     {
+        [OperationContract]
+        AuthBody Authorization(string username, string password, DateTime time);
 
+        [OperationContract]
+        AuthBody Authentication(int user_id, string access_token);
+
+        [OperationContract]
+        AuthBody UpdateToken(int user_id, string access_token, string refresh_token);
     }
 }
